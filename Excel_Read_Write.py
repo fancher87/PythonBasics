bf2d9ce746885a7afa2d8f9b539525427f1546ae
Emily <33325555+fancher87@users.noreply.github.com>
2021-01-13 15:22:33 -0800

    Update Excel_Read_Write.py
    Adding the import of multiple excel files within one path.  Use the * key as a wildcard like Unix Shell.

--- Excel_Read_Write.py
+++ Excel_Read_Write.py
@@ -22,6 +22,24 @@ with pd.ExcelWriter('Workbook_Name_Generic.xlsx') as writer:
   df2.to_excel(writer, sheet_name='Sheet2', index = False) #index False means that index column is not written, keep in mind if data is used as index rather 
   
   
+  
+  
+#import multiple different workbooks from one directory with similar path names
+genericpath = '.\Workbook_Name_Generic*.xlsx' 
+import glob
 
+all_data = pd.DataFrame()
+for f in glob.glob(genericpath):
+  df = pd.read_excel(f)
+  all_data = all_data.append(df, ignore_index=True) 
+#glob module finds all pathnames matching a specified pattern to the rules used by the Unix Shell
+# https://pbpython.com/excel-file-combine.html
+
+  
+  
+
+  
+  
+  
   
   
